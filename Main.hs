6488c62865f778fcf7c484ea6ff5b20d2c1dc73a
luke <nerorevenge@gmail.com>
2017-02-16 17:37:07 -0500

    Major code cleanup

--- Main.hs
+++ Main.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE BangPatterns #-}
+ 
 import Web.Scotty
 import Trie
 import System.Directory
@@ -16,26 +18,40 @@ defaultFileName = "trie.txt"
 dictionaryFile :: String
 dictionaryFile = "dictionary.txt"
 
-
 listIntoTrie :: [String] -> STrie
 listIntoTrie =  foldr (\x a -> insert x a ) emptyTrie 
+
+-- convenience function
+getFile :: String -> IO (Either String String)
+getFile fileName = doesFileExist dictionaryFile >>= 
+                      (\x -> case x of False -> return (Left $ fileName ++ " Not found!")
+                                       True ->  Right <$> readFile fileName)
 
 -- check if default exists
 -- if not create file
 getTrie :: IO (Either String STrie)
-getTrie = doesFileExist dictionaryFile >>=
-              (\x -> case x of False -> return $ Left "Dictionary File is missing!!!"
-                               True -> doesFileExist defaultFileName >>=
-                                       (\y -> case y of True ->  (Right . read) <$> (readFile defaultFileName) 
-                                                        False -> listIntoTrie . lines <$> readFile dictionaryFile 
-                                                                 >>= (\z -> writeFile defaultFileName (show z)
-                                                                           >> return (Right z))))
--- damn there has to be a mempty for Result
-main = do
-         trie <- getTrie
-         case trie of (Left x) -> putStrLn x 
-                      (Right y) -> scotty 3000 $ get "/:word" $ param "word" 
-                                                  >>= (\x -> if x == "" 
-                                                               then json (Result {results = []})
-                                                               else  json (Result {results = (autoComplete x y)}))
+getTrie = getFile dictionaryFile >>= 
+              either 
+                  (return . Left)
+                  (\z -> getFile defaultFileName >>= 
+                           either 
+                             (\_ -> do
+                                     let content = listIntoTrie $ lines z
+                                     writeFile defaultFileName (show content)
+                                     return (Right content))
+                             (\g -> do
+                                     let !stuff = read g
+                                     return (Right stuff)))
 
+
+-- damn there has to be a mempty for Result
+autoCompletePath :: String -> STrie -> ActionM () 
+autoCompletePath x y = if x == "" 
+                        then json (Result {results = []}) 
+                        else  json (Result {results = (autoComplete x y)})
+
+-- server routes
+routes :: STrie -> ScottyM ()
+routes x = get "/:word" $ param "word" >>= ((flip autoCompletePath) x)
+
+main = getTrie >>= either putStrLn (\x -> scotty 3000 (routes x)) 

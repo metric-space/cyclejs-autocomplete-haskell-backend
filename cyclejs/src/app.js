df1d5f50d3d6aca762af713de2ed4b19d4f89abe
luke <nerorevenge@gmail.com>
2017-03-15 01:27:42 -0400

    CLeaned up code  - documentation of stream architecture yet to come

--- cyclejs/src/app.js
+++ cyclejs/src/app.js
@@ -6,84 +6,86 @@ import {html} from 'snabbdom-jsx'
 
 const xx = "fixed"
 
-function AutoComplete(props){
+function AutoComplete(props) {
 
-  const condition = (props.data.length == 1 && props.value == props.data[0])
+    const data = (props.data || []).map((x, index) => <tr>
+        <td className={xx+index}>{x}</td>
+    </tr>)
 
-  const data = condition? [] : (props.data || []).map((x,index) => <tr><td className={xx+index}>{x}</td></tr>)
-  
-  return (<div className="container">
-	     <input type="text" id="autocomplete-input" className="autocomplete" value={props.value} />
-	     <table className="highlight">
-	      <tbody>
-	       {data}
-              </tbody>
-	     </table>
-	  </div>)
+    const input = (props.value) ?
+        <input type="text" id="autocomplete-input" className="autocomplete" value={props.value}/> :
+        <input type="text" id="autocomplete-input" className="autocomplete"/>;
+
+    return (<div className="container">
+        {input}
+        <table className="highlight">
+            <tbody>
+            {data}
+            </tbody>
+        </table>
+    </div>)
 }
 
 
-function appAtomStreams({DOM,HTTP}){
-  const http$ = HTTP.select('results')
-          .flatten()
-          .map(res => res.body.results)
-	  .startWith([])
+function appAtomStreams({DOM,HTTP}) {
+    const http$ = HTTP.select('results')
+        .flatten()
+        .map(res => res.body.results)
+        .startWith([])
 
-  const staticInputStream$ = DOM
-	  .select(".autocomplete")
-	  .events("input")
-	  .map(ev => ev.target.value)
-	  .startWith("")
+    const staticInputStream$ = DOM
+        .select(".autocomplete")
+        .events("input")
+        .map(ev => ev.target.value)
+        .startWith("");
 
-  const dynamicInputStream$ = http$.map(x => {
-        const indexes = []
-        for (var i = 0; i < x.length ; i++ ){
-             indexes.push(i)
+    const dynamicInputStream$ = http$.map(x => {
+        const indexes = [];
+        for (var i = 0; i < x.length; i++) {
+            indexes.push(i)
         }
-   
-        return indexes.map(x => DOM.select("."+xx+x).events("click"))
-	              .reduce((x,y) => xs.merge(x,y), xs.never())
-	              .map(ev => ev.srcElement.innerText)
-                      .take(1)
 
-   }).flatten()
+        return indexes.map(x => DOM.select("." + xx + x).events("click"))
+            .reduce((x, y) => xs.merge(x, y), xs.never())
+            .map(ev => ev.srcElement.innerText)
+            .take(1)
+
+    }).flatten();
 
 
-  return {http$, staticInputStream$, dynamicInputStream$};
+    return {http$, staticInputStream$, dynamicInputStream$};
 }
 
 
-function appWriteToWorld(http$, inputStream$){
+function appWriteToWorld({http$, staticInputStream$, dynamicInputStream$}) {
 
-  const httpSinkStream$ = inputStream$
+    const httpSinkStream$ = staticInputStream$
         .filter(x => x != "")
         .map(x => {
             return {
                 url: './search/' + x,
                 category: 'results',
-                method: 'GET' }
-	})
+                method: 'GET'
+            }
+        });
 
-  const vdom$ = xs.combine(inputStream$,http$)
-          .map(z => {
-	     return <AutoComplete data={z[1]} value={z[0]} />
-           })
+    const vdom$ = xs.merge(staticInputStream$, dynamicInputStream$, http$)
+        .map(z => {
+            return (typeof(z) == 'string') ? <AutoComplete value={z}/> :
+                <AutoComplete data={z}/>
+        });
 
-  return {httpSinkStream$, vdom$}; 
+    return {httpSinkStream$, vdom$};
 }
 
 
-export function App (sources) {
+export function App(sources) {
 
-  const {http$, staticInputStream$, dynamicInputStream$} = appAtomStreams(sources);
+    const {vdom$, httpSinkStream$} = appWriteToWorld(appAtomStreams(sources));
 
-  const inputStream$ = xs.merge(staticInputStream$, dynamicInputStream$);
-    
-  const {vdom$, httpSinkStream$} = appWriteToWorld(http$, inputStream$);
-
-  const sinks = {
-    DOM:  vdom$,
-    HTTP: httpSinkStream$
-  }
-  return sinks
+    const sinks = {
+        DOM: vdom$,
+        HTTP: httpSinkStream$
+    };
+    return sinks
 }
